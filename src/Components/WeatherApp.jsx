40505ab943c58a8072e509ab911a5675a60cbb5b
MONICA <monik.ferrer@gmail.com>
2024-09-02 13:48:57 +0200

    First component weather app

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -1,11 +1,13 @@
-
-
 const WeatherApp = () => {
   return (
-    <div>
-      
+    <div className="container">
+      <div className="waether-app">
+        <div className="search">
+          <div className="search-top"></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WeatherApp
+export default WeatherApp;
